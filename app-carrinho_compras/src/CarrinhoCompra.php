ad5fe81dd9b74e20976eff0ab66ac9d95c6617a8
Pablojonh6550 <pablojonh6550@gmail.com>
2024-07-03 21:53:59 -0300

    feat: add methods CarBuy

---
+++ app-carrinho_compras/src/CarrinhoCompra.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+class CarBuy
+{
+    // attributes
+    private $itens;
+    private $status;
+    private $totalValue;
+
+    // methods
+    public function __construct()
+    {
+        $this->itens = [];
+        $this->status = 'true';
+        $this->totalValue = 0;
+    }
+    public function getItens(): array
+    {
+        return $this->itens;
+    }
+
+    public function setItens(string $itens, float $value): bool
+    {
+        array_push($this->itens, ["item" => $itens, "value" => $value]);
+        return true;
+    }
+}
