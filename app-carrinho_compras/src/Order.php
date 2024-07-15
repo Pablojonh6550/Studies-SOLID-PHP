47dcd4d8aa2ec492f3449bc02f8eeeae409e5c7f
Pablojonh6550 <pablojonh6550@gmail.com>
2024-07-15 10:50:47 -0300

    feat: add methods in Order

---
+++ app-carrinho_compras/src/Order.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App;
+
+use App\CarBuy;
+
+class Order
+{
+
+    // attributes
+    private $status;
+    private $carBuy;
+    private $carValue;
+
+    // methods
+    public function __construct()
+    {
+        $this->status = 'Aberto';
+        $this->carBuy = new CarBuy();
+        $this->carValue = 0;
+    }
+
+    public function getCarBuy(): CarBuy
+    {
+        return $this->carBuy;
+    }
+
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): void
+    {
+        $this->status = $status;
+    }
+
+    public function confirm(): bool
+    {
+        if ($this->carBuy->valideteCarBuy()) {
+            $this->setStatus('Confirmado');
+            return true;
+        }
+
+        return false;
+    }
+}
