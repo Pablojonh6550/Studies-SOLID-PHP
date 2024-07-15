d98d0467a7a4ce2d50c31675ecbe071a59028186
Pablojonh6550 <pablojonh6550@gmail.com>
2024-07-15 10:49:28 -0300

    feat: add methods in carBuy

--- app-carrinho_compras/src/CarBuy.php
+++ app-carrinho_compras/src/CarBuy.php
@@ -2,56 +2,29 @@
 
 namespace App;
 
+use App\Item;
+
 class CarBuy
 {
+
     // attributes
     private $itens;
-    private $status;
-    private $totalValue;
 
     // methods
     public function __construct()
     {
         $this->itens = [];
-        $this->status = 'Aberto';
-        $this->totalValue = 0;
     }
+
     public function getItens(): array
     {
         return $this->itens;
     }
 
-    public function setItens(string $itens, float $value): bool
+    public function setItens(Item $item): bool
     {
-        array_push($this->itens, ["item" => $itens, "value" => $value]);
-        $this->totalValue += $value;
-
+        array_push($this->itens, $item);
         return true;
-    }
-
-    public function totalValueCar(): float
-    {
-        return $this->totalValue;
-    }
-
-    public function getStatus(): string
-    {
-        return $this->status;
-    }
-
-    public function confirmOrder(): bool
-    {
-        if ($this->valideteCarBuy()) {
-            $this->status = 'Confirmado';
-            $this->sendEmailConfirmation();
-            return true;
-        }
-        return false;
-    }
-
-    public function sendEmailConfirmation(): void
-    {
-        echo "<br /> ... envia email de confirmação ...";
     }
 
     public function valideteCarBuy(): int
