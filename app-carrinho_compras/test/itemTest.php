b9a51acd1f124fc6f3980ba1a27e5b47f9a92b31
Pablojonh6550 <pablojonh6550@gmail.com>
2024-08-02 12:48:29 -0300

    refactor: itens test methods

--- app-carrinho_compras/test/itemTest.php
+++ app-carrinho_compras/test/itemTest.php
@@ -1,12 +1,31 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use App\Item;
 
-class itemTest extends TestCase
+class ItemTest extends TestCase
 {
-  public function testEstouFuncionando()
-  {
-    $valor = 10;
-    $this->assertEquals(10, $valor);
-  }
+    public function testItemInstantiated()
+    {
+        $item = new Item();
+
+        // checked attributes
+        $this->assertEquals("", $item->getName());
+        $this->assertEquals(0, $item->getValue());
+    }
+
+    public function testGetAndSetItemName()
+    {
+        $item = new Item();
+        $name = "Copo de vidro";
+        $item->setName($name);
+        $this->assertEquals($name, $item->getName());
+    }
+    public function testGetAndSetItemValue()
+    {
+        $item = new Item();
+        $value = 35.90;
+        $item->setValue($value);
+        $this->assertEquals($value, $item->getValue());
+    }
 }
